877863a1600310f06fd6cd76bc11f848c6913776
REDIKAN <igore458@gmail.com>
2026-07-07 19:50:59 +0300

    Add gizmos visualization for initial blocks with display toggles

--- Assets/Scripts/Views/GridEditorVisualizer.cs
+++ Assets/Scripts/Views/GridEditorVisualizer.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR
 using UnityEngine;
+using Game.Data;
 
 namespace Game.Views
 {
@@ -10,10 +11,19 @@ namespace Game.Views
         private const float CellSize = 1f;
         private static readonly Vector3 CellOffset = new(0.5f, 0.5f, 0.5f);
 
+        [field: SerializeField] private ShadowLevelConfig Config { get; set; }
+        [field: SerializeField] private bool ShowGrid { get; set; } = true;
+        [field: SerializeField] private bool ShowInitialBlocks { get; set; } = true;
+
         private void OnDrawGizmos()
         {
-            Gizmos.color = new Color(1f, 1f, 1f, 0.2f);
+            if (ShowGrid) DrawGrid();
+            if (ShowInitialBlocks) DrawInitialBlocks();
+        }
 
+        private void DrawGrid()
+        {
+            Gizmos.color = new Color(1f, 1f, 1f, 0.2f);
             for (var x = 0; x < GridSize; x++)
                 for (var y = 0; y < GridSize; y++)
                     for (var z = 0; z < GridSize; z++)
@@ -21,6 +31,18 @@ namespace Game.Views
                         var center = new Vector3Int(x, y, z) + CellOffset;
                         Gizmos.DrawWireCube(center, Vector3.one * CellSize);
                     }
+        }
+
+        private void DrawInitialBlocks()
+        {
+            if (Config?.InitialBlocks is null) return;
+
+            Gizmos.color = new Color(0f, 1f, 0f, 0.3f);
+            foreach (var block in Config.InitialBlocks)
+            {
+                var center = block + CellOffset;
+                Gizmos.DrawCube(center, Vector3.one * CellSize);
+            }
         }
     }
 }
