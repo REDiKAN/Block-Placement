823f2224fe5985c892e0a8a94d1864ca29308b53
REDIKAN <igore458@gmail.com>
2026-09-04 14:13:37 +0300

    Fix Rotate

--- Assets/Scripts/Core/Services/Placement/StructurePlacementService.cs
+++ Assets/Scripts/Core/Services/Placement/StructurePlacementService.cs
@@ -6,6 +6,7 @@ using Game.Services.Input;
 using Game.Services.Pool;
 using Game.Services.Raycast;
 using Game.Services.Registry;
+using Game.Services.Rotation;
 using Game.Views;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,7 @@ namespace Game.Services.Placement
     public class StructurePlacementService : IStructurePlacementService, IInitializable, IDisposable
     {
         public IObservable<Unit> OnGridChanged => _onGridChanged;
+
         private readonly Subject<Unit> _onGridChanged = new();
         private readonly CompositeDisposable _disposables = new();
 
@@ -38,6 +40,7 @@ namespace Game.Services.Placement
         private readonly ISfxService _sfxService;
         private readonly AudioConfig _audioConfig;
         private readonly LevelConfig _levelConfig;
+        private readonly IRotationService _rotationService;
 
         private StructureConfig _selectedConfig;
         private StructureView _previewInstance;
@@ -54,7 +57,8 @@ namespace Game.Services.Placement
             IObjectRegistryService registryService,
             ISfxService sfxService,
             AudioConfig audioConfig,
-            LevelConfig levelConfig)
+            LevelConfig levelConfig,
+            IRotationService rotationService)
         {
             _inputService = inputService;
             _raycastService = raycastService;
@@ -66,6 +70,7 @@ namespace Game.Services.Placement
             _sfxService = sfxService;
             _audioConfig = audioConfig;
             _levelConfig = levelConfig;
+            _rotationService = rotationService;
         }
 
         public void Initialize()
@@ -73,6 +78,8 @@ namespace Game.Services.Placement
             _inputService.OnMouseMoved.Subscribe(UpdatePreview).AddTo(_disposables);
             _inputService.OnPrimaryClick.Subscribe(PlaceStructure).AddTo(_disposables);
             _inputService.OnSecondaryClick.Subscribe(_ => RemoveLastStructure()).AddTo(_disposables);
+
+            _rotationService.OnRotationCompleted.Subscribe(RotateActiveStructures).AddTo(_disposables);
         }
 
         public void SelectStructure(StructureConfig config)
@@ -195,9 +202,7 @@ namespace Game.Services.Placement
             if (_activeStructures.TryGetValue(record.Cells[0], out var structure))
             {
                 _activeStructures.Remove(record.Cells[0]);
-
                 structure.gameObject.SetActive(false);
-
                 _isAnimating = true;
                 _contextService.SetContext(InputContext.Generating);
                 Observable.Timer(TimeSpan.FromSeconds(0.1f))
@@ -216,15 +221,42 @@ namespace Game.Services.Placement
             _onGridChanged.OnNext(Unit.Default);
         }
 
+        private void RotateActiveStructures(int angle)
+        {
+            var gridSize = _gridService.GridSize;
+            var newActiveStructures = new Dictionary<Vector3Int, StructureView>();
+
+            foreach (var kvp in _activeStructures)
+            {
+                var origin = kvp.Key;
+                var structure = kvp.Value;
+
+                var newOrigin = angle == 90
+                    ? new Vector3Int(origin.z, origin.y, gridSize - 1 - origin.x)
+                    : new Vector3Int(gridSize - 1 - origin.z, origin.y, origin.x);
+
+                structure.SetPosition(newOrigin);
+                newActiveStructures[newOrigin] = structure;
+            }
+
+            _activeStructures.Clear();
+            foreach (var kvp in newActiveStructures)
+                _activeStructures.Add(kvp.Key, kvp.Value);
+
+            _onGridChanged.OnNext(Unit.Default);
+        }
+
         private bool ValidatePlacement(Vector3Int origin, Vector3Int[] localCoords)
         {
             if (localCoords is null || localCoords.Length == 0) return false;
             var hasSupport = false;
+
             foreach (var local in localCoords)
             {
                 var worldCell = origin + local;
                 if (!_gridService.IsWithinBounds(worldCell)) return false;
                 if (_gridService.IsCellOccupied(worldCell)) return false;
+
                 if (worldCell.y == 0)
                 {
                     var floorCoord = new Vector2Int(worldCell.x, worldCell.z);
@@ -236,6 +268,7 @@ namespace Game.Services.Placement
                     if (_gridService.IsCellOccupied(belowCell)) hasSupport = true;
                 }
             }
+
             return hasSupport;
         }
 
--- Assets/Scripts/Views/StructureView.cs
+++ Assets/Scripts/Views/StructureView.cs
@@ -7,15 +7,26 @@ namespace Game.Views
         private static readonly Vector3 Offset = new(0.5f, 0.5f, 0.5f);
         private Collider[] _colliders;
 
-        private void Awake() => _colliders = GetComponentsInChildren<Collider>();
+        private void Awake()
+        {
+            _colliders = GetComponentsInChildren<Collider>();
+        }
+
+        private void OnEnable()
+        {
+            transform.localRotation = Quaternion.identity;
+        }
 
         public void SetPosition(Vector3Int cell) => transform.position = cell + Offset;
 
         public void SetInteractionEnabled(bool enabled)
         {
             if (_colliders is null) return;
+
             foreach (var c in _colliders)
+            {
                 if (c is not null) c.enabled = enabled;
+            }
         }
     }
 }
